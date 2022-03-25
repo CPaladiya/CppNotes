97a9a5bcbaca0bf618e3e30077396f0a591f66f5
Chirag Paladiya <chirag.paladiya@hotmail.com>
2022-03-25 13:16:02 -0700

    Added : Check endian explanation added

--- 1_Numbers.cpp
+++ 1_Numbers.cpp
@@ -1,7 +1,8 @@
 #include "1_Numbers.hpp"
 /*  What's covered?
 ** (1) How to check if a header is available? and if yes, how to conditionally use it?
-** (2) Never attach 0 in from of a value, why?
+** (2) Never attach 0 in from of a value (int of float), why?
+** (3) How to know if my program is compiled for big - or little- endian?
 */
 
 //** (1) ****************************//
@@ -15,13 +16,56 @@ void HeaderCheck(){
 
 //** (2) ****************************//
 void OctalValues(){
+    //this applies to ints and floats as well
     int i = 100, j = 0100;
-    cout << " Values of i " << i << " and j " << j << endl;
-    cout << " This happens because when we attach 0 ahead of j, \nwe request to read in \"Octal Values\" instead of \"Decimal Values\"" << endl;
+    float k = 0100;
+    cout << " Values of 'int i = 100' : i is really " << i << ", and 'int i = 0100' : i is really " << j << endl;
+    cout << " This happens because when we attach 0 ahead of j, \n we request to read in \"Octal Values\" instead of \"Decimal Values\"" << endl;
 }//(2)
 
+//** (3) ****************************//
+void CheckEndian(){
+    
+    // little endian : least significant digit of a number stored first (lower mem address) following the most significant digit (higher mem address)
+    // big endian : most significant digit of a number stored first (lower mem address) following the less significant digit (higher mem address)
+    
+    // let's see the example of a number 513, 4 bytes of memory is assigned to the variable (binary of 513): 0000,0000   0000,0000    0000,0010   0000,0001
+    // notice that inside 8-bit array - or a single byte ALWAYS implements big endian, meaning least significant to right and most significant to left
 
+    //Little endian encoding
+    /* mem adress : 00               01              02                  03
+       values     : 0000,0001        0000,0010       0000,0000           0000,0000*/
+
+    //Big endian encoding
+    /* mem adress : 00               01              02                  03
+       values     : 0000,0000        0000,0000       0000,0010           0000,0001*/
+
+    #ifdef __cpp_lib_bit //only run this if cpp has bit header c++20
+
+    if(std::endian::native == std::endian::little) { cout << "This system is compiling for little endian" << endl;}
+    else if(std::endian::native == std::endian::big) { cout << "This system is compiling for big endian" << endl;}
+
+    #else
+
+    cout << " ---------Testing endian the old school way!------------- " << endl;
+    short int word = 0x0001; // assigning some value to short int
+    char *b = (char*) &word;
+    cout << " Mem size of the word : " << sizeof(word) << " Bytes, Binary value of 'Word' : " << std::bitset<8>(word) << endl;
+    cout << " Out of " << sizeof(word) << " bytes, we will fetch the address of the first byte : " << &b   
+    << " \n 1) If start char address contains value of 1, that is least significant digit is at the start so, it is little-endian encoding."   
+    << "\n 2) If start char address contains value of 0, that is least significant digit is not at the start so, it is big-endian encoding." << endl;
+
+    cout << " b[0] Value: " << (b[0] ? 1 : 0) <<(b[0] ? " : Little Endian " : " : Big Endian ") << endl;
+
+    // Q : What is the reason most os implements little-endian encoding?
+    /* A : Well, even if we assign let's say 4 bytes to a variable we don't always use the full capacity,
+    * Most of the numbers will have 0s in their significant digit space. With big endian encoding, 
+    * we traverse from most significant digit to least, we traverse through 0s first most of the time. 
+    * >>> To make it more efficient, we can traverse through least digit first, which will most likely won't be zero.
+    */
+
+    #endif
+}//(3)
 
 int main(){
-    OctalValues();
 }
--- 1_Numbers.hpp
+++ 1_Numbers.hpp
@@ -1,16 +1,27 @@
 /*  What's covered?
 ** (1) How to check if a header is available? and if yes, how to conditionally use it?
-** (2) Never attach 0 in from of a value, why?
+** (2) Never attach 0 in from of a value (int of float), why?
+** (3) How to know if my program is compiled for big - or little- endian?
 */
+#include <iostream>
+#include <bitset>
 
-//(1) **************************** notice how one can check if there is header file avaliable //
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Special def cases ^^^^^^^^^^^^^^^^^ //
+
+//(1) ************************ notice how one can check if there is header file avaliable //
 #if __has_include(<format>)
 #include <format>
 #endif //(1)
 
-#include <iostream>
+//(3) ************************ header to check big/little endian //
+#if __has_include(<bit>)
+#include <bit>
+#endif //(3)
 
 using namespace std;
 
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Clarification Funcs ^^^^^^^^^^^^^^^^^ //
+
 void HeaderCheck(); //(1)
 void OctalValues(); //(2)
+void CheckEndian(); //(3)
